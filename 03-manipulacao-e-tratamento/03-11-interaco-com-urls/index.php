9c32eb6b0d532dd9b98c906b75697d7229fb25f2
Pedro <pedrocavt@gmail.com>
2023-02-13 09:16:10 -0300

    interacao url

--- 03-manipulacao-e-tratamento/03-11-interaco-com-urls/index.php
+++ 03-manipulacao-e-tratamento/03-11-interaco-com-urls/index.php
@@ -8,8 +8,44 @@ fullStackPHPClassName("03.11 - Interação com URLs");
 fullStackPHPClassSession("argumentos", __LINE__);
 
 
+echo "<h1><a href='index.php'>Clear</a></h1>";
+
+$data = [
+    'name' => "Pedro",
+    "email" => "pedrocavt@gmail.com"
+];
+
+$arguments = http_build_query($data);
+echo "<h1><a href='index.php?{$arguments}'>Args By Array</a></h1>";
+
+var_dump($_GET);
+
 /*
  * [ segurança ] get | strip | filters | validation
  * [ filter list ] https://php.net/manual/en/filter.filters.php
  */
 fullStackPHPClassSession("segurança", __LINE__);
+
+$dataFilter = http_build_query([
+    'name' => "Pedro",
+    "email" => "pedrocavt@gmail.com",
+    "script" => "<script>alert('se fudeu')</script>",
+    "site" => "http://pedro.com.br"
+]);
+
+echo "<h1><a href='index.php?{$dataFilter}'>Data Filter</a></h1>";
+
+$dataUrl = filter_input_array(INPUT_GET, FILTER_SANITIZE_SPECIAL_CHARS);
+
+$filter = [
+    'name' => FILTER_SANITIZE_SPECIAL_CHARS,
+    "email" => FILTER_VALIDATE_EMAIL,
+    "script" => FILTER_SANITIZE_SPECIAL_CHARS,
+    "site" => FILTER_VALIDATE_URL
+];
+
+$dataUrlFilter = filter_input_array(INPUT_GET, $filter);
+
+var_dump($dataUrl);
+
+var_dump($dataUrlFilter);
