6cbbb6c3e1cdbc200559faa4b8be4ce08d956a4d
Pedro <pedrocavt@gmail.com>
2023-02-13 09:52:44 -0300

    cookies and sessions

--- 03-manipulacao-e-tratamento/03-12-cookies-e-sessoes/index.php
+++ 03-manipulacao-e-tratamento/03-12-cookies-e-sessoes/index.php
@@ -7,8 +7,60 @@ fullStackPHPClassName("03.12 - Cookies e sessões");
  */
 fullStackPHPClassSession("cookies", __LINE__);
 
+// setcookie("php", "esse é meu cookie", time() + 60);
+// setcookie("php", '', time() - 60); //delete
+
+$cookie = filter_input_array(INPUT_COOKIE, FILTER_SANITIZE_SPECIAL_CHARS);
+
+$time = time() + 60 * 60 * 24 * 7; //Uma semana
+$user = [
+    "user" => "root",
+    "password" => "1234",
+    "expire" => $time
+];
+
+setcookie(
+    "fslogin",
+    http_build_query($user),
+    $time,
+    "/",
+    "localhost",
+    true
+);
+
+$login = filter_input(INPUT_COOKIE, 'fslogin', FILTER_SANITIZE_SPECIAL_CHARS);
+
+if ($login) {
+    var_dump($login);
+    parse_str($login, $user);
+    var_dump($user);
+}
+
 
 /*
  * [ sessões ] http://php.net/manual/pt_BR/ref.session.php
  */
 fullStackPHPClassSession("sessões", __LINE__);
+
+session_save_path(__DIR__ . "/ses");
+
+session_start([
+    "cookie_lifetime" => (60 * 60 * 24)
+]);
+
+var_dump(
+[
+    "id" => session_id(),
+    "name" => session_name(),
+    "status" => session_status(),
+    "save_path" => session_save_path(),
+    "cookie" => session_get_cookie_params()
+]);
+
+// $_SESSION['login'] = (object) $user;
+// $_SESSION['user'] = $user;
+// unset($_SESSION['user']);
+
+session_destroy();
+
+var_dump($_SESSION);
